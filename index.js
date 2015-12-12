8e40c50a2ffe1a5ab0aac4d0c6f5e4940c783994
Ryan Clark <ryan.clark.j@gmail.com>
2015-12-11 17:48:21 -0700

    overhaul

--- index.js
+++ index.js
@@ -1,138 +1,165 @@
-var AWS = require('aws-sdk');
 var stream = require('stream');
-var util = require('util');
 
-// required config:
-// - name
-// - region
-//
-// optional config:
-// - endpoint
-// - accessKeyId
-// - secretAccesKey
-// - sessionToken
+module.exports = KinesisReadable;
 
-module.exports = function(config) {
-  var kinesis = new AWS.Kinesis({
-    endpoint: config.endpoint,
-    accessKeyId: config.accessKeyId,
-    secretAccessKey: config.secretAccessKey,
-    sessionToken: config.sessionToken,
-    region: config.region
-  });
-
-  util.inherits(KinesisReadable, stream.Readable);
-
-  // Optional:
-  // - shardId: to read from
-  // - latest: to only read records written after you start listening
-  // - lastCheckpoint: start reading the record after the given sequence number
-  // - limit: max number of records that will be passed to `data` event
-  function KinesisReadable(options) {
-    options = options || {};
-
-    this.name = config.name;
-    this.shardId = options.shardId;
-    this.kinesis = kinesis;
-    this.iterator = null;
-    this.pending = 0;
-    this.latest = !!options.latest;
-    this.lastCheckpoint = options.lastCheckpoint;
-    this.limit = Math.min(10000, options.limit) || 1;
-
-    stream.Readable.call(this, { objectMode: true });
+/**
+ * A factory to generate a {@link KinesisClient} that pulls records from a Kinesis stream
+ *
+ * @param {object} client - an AWS.Kinesis client capable of reading the desired stream
+ * @param {string} [name] - the name of the shard to read. Not required if already
+ * set by the provided AWS.Kinesis client.
+ * @param {object} [options] - configuration details
+ * @param {string} [options.shardId] - the shard id to read from. Each KinesisReadable
+ * instance is only capable of reading a single shard. If unspecified, the instance
+ * will read from the first shard returned by a DescribeStream request.
+ * @param {string} [options.iterator] - the iterator type. One of `LATEST` or `TRIM_HORIZON`.
+ * If unspecified, defaults to `TRIM_HORIZON`
+ * @param {string} [options.startAt] - a sequence number to start reading from.
+ * @param {string} [options.startAfter] - a sequence number to start reading after.
+ * @param {number} [options.limit] - the maximum number of records that will
+ * be passed to any single `data` event.
+ * @returns {KinesisClient} a readable stream of kinesis records
+ */
+function KinesisReadable(client, name, options) {
+  if (typeof name === 'object') {
+    options = name;
+    name = undefined;
   }
 
-  KinesisReadable.prototype.close = function(callback) {
-    callback = callback || function() {};
-    var _this = this;
+  if (!options) options = {};
 
-    _this.drain = true;
-    if (_this.pending) return setImmediate(_this.close.bind(_this), callback);
+  if (options.iterator && options.iterator !== 'LATEST' && options.iterator !== 'TRIM_HORIZON')
+    throw new Error('options.iterator must be one of LATEST or TRIM_HORIZON');
 
-    _this.once('end', callback);
+  var readable = new stream.Readable({
+    objectMode: true,
+    highWaterMark: 100
+  });
 
-    _this.push(null);
-    _this.resume();
-  };
+  var checkpoint = new stream.Transform({
+    objectMode: true,
+    highWaterMark: 100
+  });
 
-  KinesisReadable.prototype._read = function read() {
-    var _this = this;
-    if (_this.drain) return;
-    if (!_this.iterator) return _this._describe(haveIterator);
-    haveIterator();
+  var iterator, drain, ended, pending = 0;
 
-    function haveIterator(err) {
-      if (_this.drain) return;
-      if (err) return _this.emit('error', err);
+  function describeStream(callback) {
+    pending++;
+    client.describeStream({ StreamName: name }, function(err, data) {
+      pending--;
+      if (err) return callback(err);
 
-      _this.pending++;
-      kinesis.getRecords({
-        ShardIterator: _this.iterator,
-        Limit: _this.limit
-      }, function(err, data) {
-        _this.pending--;
-        if (err) return _this.emit('error', err);
+      var shardId = options.shardId ?
+        data.StreamDescription.Shards.filter(function(shard) {
+          return shard.ShardId === options.shardId;
+        }).map(function(shard) {
+          return shard.ShardId;
+        })[0] : data.StreamDescription.Shards[0].ShardId;
 
-        _this.iterator = data.NextShardIterator;
+      if (!shardId) return callback(new Error('Shard ' + options.shardId + ' does not exist'));
+      getShardIterator(shardId, callback);
+    });
+  }
 
-        if (!data.Records.length) return setImmediate(function() {
-          _this._read();
-        });
+  function getShardIterator(shardId, callback) {
+    var params = {
+      ShardId: shardId,
+      StreamName: name
+    };
 
-        _this.push(data.Records);
-        _this.emit('checkpoint', data.Records.slice(-1)[0].SequenceNumber);
-      });
+    if (options.iterator) {
+      params.ShardIteratorType = options.iterator;
+    } else if (options.startAt) {
+      params.ShardIteratorType = 'AT_SEQUENCE_NUMBER';
+      params.StartingSequenceNumber = options.startAt;
+    } else if (options.startAfter) {
+      params.ShardIteratorType = 'AFTER_SEQUENCE_NUMBER';
+      params.StartingSequenceNumber = options.startAfter;
+    } else {
+      params.ShardIteratorType = 'TRIM_HORIZON';
+    }
+
+    pending++;
+    client.getShardIterator(params, function(err, data) {
+      pending--;
+      if (err) return callback(err);
+      iterator = data.ShardIterator;
+      callback();
+    });
+  }
+
+  function read(callback) {
+    if (drain && !pending) return callback(null, { Records: null });
+    if (drain && pending) return setImmediate(read, callback);
+
+    pending++;
+    client.getRecords({
+      ShardIterator: iterator,
+      Limit: options.limit
+    }, function(err, data) {
+      pending--;
+      if (err) return callback(err);
+
+      iterator = data.NextShardIterator;
+
+      if (!data.Records.length) {
+        if (!drain) return setTimeout(read, 200, callback);
+        data.Records = null;
+      }
+
+      callback(null, data);
+    });
+  }
+
+  readable._read = function() {
+    if (iterator) return read(gotRecords);
+
+    describeStream(function(err) {
+      if (err) return checkpoint.emit('error', err);
+      read(gotRecords);
+    });
+
+    function gotRecords(err, data) {
+      if (err) return checkpoint.emit('error', err);
+      setTimeout(readable.push.bind(readable), 200, data.Records);
     }
   };
 
-  KinesisReadable.prototype._describe = function describe(callback) {
-    var _this = this;
-    if (_this.drain) return callback();
-
-    _this.kinesis.describeStream({
-      StreamName: _this.name
-    }, function(err, data) {
-      if (err) return callback(err);
-      if (_this.drain) return callback();
-
-      if (typeof _this.shardId === 'string') {
-        var match = data.StreamDescription.Shards.filter(function(shard) {
-          return shard.ShardId === _this.shardId;
-        });
-        if (match.length === 0)
-          return callback(new Error('Shard ' + _this.shardId + ' does not exist'));
-      } else {
-        _this.shardId = data.StreamDescription.Shards[0].ShardId;
-      }
-
-      var params = {
-        StreamName: _this.name,
-        ShardId: _this.shardId,
-        ShardIteratorType: 'AT_SEQUENCE_NUMBER',
-        StartingSequenceNumber: data.StreamDescription.Shards[0].SequenceNumberRange.StartingSequenceNumber
-      };
-
-      if (_this.latest) {
-        params.ShardIteratorType = 'LATEST';
-        delete params.StartingSequenceNumber;
-        _this.latest = false;
-      }
-
-      if (_this.lastCheckpoint) {
-        params.ShardIteratorType = 'AFTER_SEQUENCE_NUMBER';
-        params.StartingSequenceNumber = _this.lastCheckpoint;
-        _this.lastCheckpoint = false;
-      }
-
-      kinesis.getShardIterator(params, function(err, data) {
-        if (err) return callback(err);
-        if (_this.drain) return callback();
-        _this.iterator = data.ShardIterator;
-        callback();
-      });
-    });
+  checkpoint._transform = function(data, enc, callback) {
+    checkpoint.emit('checkpoint', data.slice(-1)[0].SequenceNumber);
+    callback(null, data);
   };
 
-  return KinesisReadable;
-};
+  checkpoint._flush = function(callback) {
+    ended = true;
+    callback();
+  };
+
+  /**
+   * A kinesis stream persists beyond the duration of a readable stream. In order
+   * to stop reading from the stream, call `.close()`. Then listen for the `end`
+   * event to indicate that all data that as been read from Kinesis has been passed
+   * downstream.
+   *
+   * @instance
+   * @memberof KinesisClient
+   * @returns {KinesisClient}
+   */
+  checkpoint.close = function() {
+    drain = true;
+    if (!ended) readable._read();
+    return checkpoint;
+  };
+
+  /**
+   * A client that implements a node.js readable stream interface for reading kinesis
+   * records. See node.js documentation for details.
+   *
+   * In addition to the normal events emitted by a readable stream, the KinesisClient
+   * emits `checkpoint` events, which indicate the most recent sequence number that
+   * has been read from Kinesis and passed downstream.
+   *
+   * @name KinesisClient
+   */
+  return readable.pipe(checkpoint);
+}
